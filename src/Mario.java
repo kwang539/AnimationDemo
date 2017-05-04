33196e35cabc674afa207d0cc12a138413b1cf4f
kwang539 <kwang539@PR04.hhs.win.fuhsd.org>
2017-05-04 13:45:28 -0700

    Add all files from the Animation Demo and made Mario fall.

---
+++ src/Mario.java
@@ -0,0 +1,52 @@
+
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.*;
+import java.util.*;
+
+public class Mario extends MovingImage {
+
+	public static final int MARIO_WIDTH = 40;
+	public static final int MARIO_HEIGHT = 60;
+	
+	private double vX, vY;
+	
+	
+	public Mario(int x, int y) {
+		super("mario.png", x, y, MARIO_WIDTH, MARIO_HEIGHT);
+		
+		vX = 0;
+		vY= 0;
+		
+	}
+
+	// METHODS
+	public void walk(int dir) {
+		if(dir == 1)
+		super.x += 1;
+		else if(dir == -1){
+			super.x -= 1;
+
+		}
+		// WALK!
+	}
+
+	public void jump() {
+		// JUMP!
+	}
+	
+	//stuff that happens with you not needing to hit a key ( passive acts)
+	public void act(ArrayList<Shape> obstacles) {
+		
+		
+		vY += .5;
+		
+		super.y += vY;
+		
+		
+		// FINISH ME!
+	}
+
+
+}
